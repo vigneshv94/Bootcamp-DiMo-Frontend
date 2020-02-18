1639b6ba62cb3a2a7291bc8ed0d90a111cefc208
Vignesh Velu <vigneshv94@gmail.com>
2020-02-18 15:47:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const path = require('path');
 
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+app.use(allowCrossDomain);
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/movieBash'));
 
@@ -12,6 +28,7 @@ app.get('*', function(req,res) {
     
 // res.sendFile(path.join(__dirname,'/dist/movieBash/index.html'));
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080)
