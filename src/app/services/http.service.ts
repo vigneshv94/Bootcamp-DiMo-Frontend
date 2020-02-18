8d44e47d61dd2787f8b01ae19f5f0ef9d3509f1d
Vignesh Velu <vigneshv94@gmail.com>
2020-02-18 15:49:15 +0530

    Update http.service.ts

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -9,7 +9,7 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class HttpService {
   // TODO: load from env
-  private REST_API_SERVER = 'https://dimo-services.herokuapp.com';
+  private REST_API_SERVER = 'https://dimo-wildwolves.herokuapp.com';
   // private REST_API_SERVER = "http://localhost:3000";
 
   constructor(private httpClient: HttpClient) {}
