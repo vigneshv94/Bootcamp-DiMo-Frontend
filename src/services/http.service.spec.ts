6f40f7e503ab4950264262f53d9c721cd5130887
Ankit Kumar <ankit_sony3@hotmail.com>
2020-02-18 10:46:51 +0530

    <Ankit Kumar, Kartik> http service

--- src/services/http.service.spec.ts
+++ src/services/http.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { HttpService } from './http.service';
+import { HttpParams } from '@angular/common/http';
 
 describe('HttpService', () => {
   let service: HttpService;
@@ -18,11 +19,11 @@ describe('HttpService', () => {
   });
 
   it('should have Get method', () => {
-    expect(service.Get()).toBeTruthy();
+    expect(service.Get('/users', new HttpParams().set('name', 'Kartik'))).toBeTruthy();
   });
 
   it('should have Post method', () => {
-    expect(service.Post({})).toBeTruthy();
+    expect(service.Post<any, {}>('/users', {name: 'Kartik'})).toBeTruthy();
   });
 
   it('should have Patch method', () => {
--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HttpService {
-  private REST_API_SERVER = 'http://localhost:3000';
+  private REST_API_SERVER = "https://api.github.com";
+  // private REST_API_SERVER = "http://localhost:3000";
 
   constructor(private httpClient: HttpClient) {}
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    // TODO: change it
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
   /**
    * Get
    */
-  public Get<Response>(): Response {
-    return ([] as unknown) as Response;
+  public Get<Response>(path: string, params: HttpParams): Observable<Response> {
+    return this.httpClient
+      .get<Response>(this.REST_API_SERVER + path, {
+        params
+      })
+      .pipe(retry(3), catchError(this.handleError));
   }
 
   /**
    * Post
    */
-  public Post<Response, Payload>(data: Payload): Response {
-    return ([] as unknown) as Response;
+  public Post<Response, Payload>(
+    path: string,
+    data: Payload
+  ): Observable<Response> {
+    return this.httpClient
+      .post<Response>(this.REST_API_SERVER + path, data)
+      .pipe(retry(3), catchError(this.handleError));
   }
 
   /**
@@ -29,5 +56,4 @@ export class HttpService {
   public Patch<Response, Payload>(data: Payload): Response {
     return ([] as unknown) as Response;
   }
-
 }
